7f984c66d6bb54dcf0e33655a449798e6df3cd4f
Thomas Woolley <tom.woolley04@gmail.com>
2025-12-03 10:30:58 +0000

    day 1 secdond part added

--- Day_01/day_01.py
+++ Day_01/day_01.py
@@ -3,16 +3,20 @@ import pandas as pd
 df = pd.read_csv("Day_01/d01_input.txt", header=None, index_col=None )
 data = df.to_numpy()
 current = 50
-count = 0
+count_part_1 = 0
+count_part_2 = 0
 
 for row in data:
     l = list(str(list(row)).strip("[']"))
     i = int(str(list(row)).strip("[']RL"))
     if l[0] == 'R':
+        count_part_2 += (current+i)//100
         current = (current+i)%100
     else:
-        current = (current-i)%100
+        count_part_2 -= (current-i)//100
+        current = (current-i)%100            
     if current == 0:
-        count += 1
+        count_part_1 += 1
     
-print(count)
+print("Part 1 answer: " + str(count_part_1))
+print("Part 2 answer: " + str(count_part_2))
