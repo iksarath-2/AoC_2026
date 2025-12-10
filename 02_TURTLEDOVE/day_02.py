e498f1b2581cafacd6acba8c6d5ad2bc8642559d
Thomas Woolley <tom.woolley04@gmail.com>
2025-12-10 14:34:25 +0000

    day_02 complete

--- 02_TURTLEDOVE/day_02.py
+++ 02_TURTLEDOVE/day_02.py
@@ -6,9 +6,15 @@ id_sum = 0
 
 for id_pair in data[0]:
     [first_id, last_id] = id_pair.split('-')
-    range = [*range(int(first_id),int(last_id)+1)]
-    for id in range:
-        if len(str(id))%2 == 0 and int(str(id)[0:len(str(id))//2]) == int(str(id)[(len(str(id))//2):len(str(id))]):
-                id_sum += id
-                
-print(str(id_sum))
+    id = first_id
+    while int(id) <= int(last_id):
+        added = False
+        for n in range(1, len(id)):
+            if len(id)%n == 0 and not added:
+                allowed = all([id[0:n] == id[x:x+n] for x in range(0,len(id),n)])
+                if allowed:
+                    id_sum += int(id)
+                    added = True
+        id = str(int(id)+1)
+        
+print(id_sum)
